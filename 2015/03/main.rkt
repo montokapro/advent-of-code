51c3d0a4c6509f7a3bc11279248c7f2900bd94be
Stephen Webber <montokapro@gmail.com>
2020-12-15 16:29:55 -0600

    02 - part 2

--- 2015/03/main.rkt
+++ 2015/03/main.rkt
@@ -13,31 +13,47 @@
 
 (struct/lens point (x y) #:transparent)
 
+(define (move char pos)
+  (cond
+    ((eqv? left-char char)
+     (lens-transform point-x-lens pos (λ (x) (- x 1))))
+    ((eqv? right-char char)
+     (lens-transform point-x-lens pos (λ (x) (+ x 1))))
+    ((eqv? down-char char)
+     (lens-transform point-y-lens pos (λ (y) (- y 1))))
+    ((eqv? up-char char)
+     (lens-transform point-y-lens pos (λ (y) (+ y 1))))
+    (#t pos)))
+
 (define (part-1 str)
   (define chars (string->list str))
-  (struct acc (position houses))
+  (struct acc (position houses) #:transparent)
   (define (f a b)
-    (define new-pos
-      (cond
-        ((eqv? left-char a)
-         (lens-transform point-x-lens (acc-position b) (λ (x) (- x 1))))
-        ((eqv? right-char a)
-         (lens-transform point-x-lens (acc-position b) (λ (x) (+ x 1))))
-        ((eqv? down-char a)
-         (lens-transform point-y-lens (acc-position b) (λ (y) (- y 1))))
-        ((eqv? up-char a)
-         (lens-transform point-y-lens (acc-position b) (λ (y) (+ y 1))))
-        (#t b)))
-    (acc new-pos (set-add (acc-houses b) new-pos)))
+    (define pos (move a (acc-position b)))
+    (acc pos (set-add (acc-houses b) pos)))
   (define result (foldl f (acc (point 0 0) (set (point 0 0))) chars))
   (set-count (acc-houses result)))
 
 (define (part-2 str)
-  (part-1 str))
+  (define chars (string->list str))
+  (struct acc (positions houses) #:transparent)
+  (define (f a b)
+    (define pos (move a (car (acc-positions b))))
+    (acc
+     (append (cdr (acc-positions b)) (list pos))
+     (set-add (acc-houses b) pos)))
+  (define result
+    (foldl
+     f
+     (acc
+      (list (point 0 0) (point 0 0))
+      (set (point 0 0)))
+     chars))
+  (set-count (acc-houses result)))
 
 (define input (file->string "input.txt"))
 
 (list
   (part-1 input)
-  ;(part-2 input)
+  (part-2 input)
 )
