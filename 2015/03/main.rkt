f8f4c9710def91aca60df1b21d3db00efa30eb24
Stephen Webber <montokapro@gmail.com>
2020-12-15 14:49:43 -0600

    Use lens library

--- 2015/03/main.rkt
+++ 2015/03/main.rkt
@@ -2,6 +2,8 @@
 
 (require racket/set)
 (require racket/file)
+(require lens/common)
+(require lens/data/struct)
 (provide (all-defined-out))
 
 (define left-char #\<)
@@ -9,7 +11,7 @@
 (define down-char #\v)
 (define up-char #\^)
 
-(struct point (x y) #:transparent)
+(struct/lens point (x y) #:transparent)
 
 (define (part-1 str)
   (define chars (string->list str))
@@ -17,14 +19,17 @@
   (define (f a b)
     (define new-pos
       (cond
-        ((eqv? left-char a) (point (- (point-x (acc-position b)) 1) (point-y (acc-position b))))
-        ((eqv? right-char a) (point (+ (point-x (acc-position b)) 1) (point-y (acc-position b))))
-        ((eqv? down-char a) (point (point-x (acc-position b)) (- (point-y (acc-position b)) 1)))
-        ((eqv? up-char a) (point (point-x (acc-position b)) (+ (point-y (acc-position b)) 1)))
+        ((eqv? left-char a)
+         (lens-transform point-x-lens (acc-position b) (λ (x) (- x 1))))
+        ((eqv? right-char a)
+         (lens-transform point-x-lens (acc-position b) (λ (x) (+ x 1))))
+        ((eqv? down-char a)
+         (lens-transform point-y-lens (acc-position b) (λ (y) (- y 1))))
+        ((eqv? up-char a)
+         (lens-transform point-y-lens (acc-position b) (λ (y) (+ y 1))))
         (#t b)))
     (acc new-pos (set-add (acc-houses b) new-pos)))
   (define result (foldl f (acc (point 0 0) (set (point 0 0))) chars))
-
   (set-count (acc-houses result)))
 
 (define (part-2 str)
